74b586c466414bbde0aa8df867606568dd161420
Ruben Alliet <allietruben@gmail.com>
2018-05-01 16:54:25 +0200

    api request on change amount

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -23,7 +23,11 @@ export default class Search extends Component {
     };
 
     onAmountChange = (e,index,value) => {
-        this.setState({'amount': value })
+        this.setState({'amount': value }, () => {
+            axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`)
+                .then(res => this.setState({'images': res.data.hits}))
+                .catch(err => console.log(err));
+        })
     } 
 
     render() {
